e9cab9d343a198773f63e2c8b2c78f64c6899276
Louise Demean V. Balili <ldbalili@svc.local>
2025-03-14 10:34:22 +0800

    floating icon img working

--- popup/content.js
+++ popup/content.js
@@ -7,30 +7,40 @@ div.style.bottom = '20px';
 div.style.right = '20px'; 
 div.style.zIndex = '9999'; 
 div.style.cursor = 'pointer';
-div.style.width = "38px";
-div.style.height = "38px";
+div.style.width = "36px";
+div.style.height = "36px";
 div.style.backgroundColor = 'white';
 div.style.borderRadius = "50%";
 div.style.border = "solid";
 div.style.borderColor = "grey";
+div.style.overflow = "hidden";
+div.style.display = "flex";
 
-img.src = "https://i.postimg.cc/Wb8cmdTk/SVC-Logo.png";
+img.src = "https://images2.imgbox.com/fc/ff/JwCxuWld_o.png";
 img.alt = "SVC Extension Button";
-img.style.width = "24px";
-img.style.height = "20px";
+img.style.width = "26px";
+img.style.height = "auto";
 img.style.overflow = "hidden";
-img.style.marginLeft = "4px";
-img.style.marginTop = "4.5px";
+img.style.position = "absolute";
+img.style.top = "50%";
+img.style.left = "50%";
+img.style.transform = "translate(-50%, -50%)";
+
+button.style.all = "unset";
+button.style.width = "100%";
+button.style.height = "100%";
+button.style.background = "transparent";
+button.style.border = "none";
 
 // Check if image is loaded correctly
-img.onload = function() {
-  console.log("Image loaded successfully.");
-};
+// img.onload = function() {
+//   console.log("Image loaded successfully.");
+// };
 
-img.onerror = function() {
-  console.error("Error loading image. Check the image source.");
-  img.alt = "Image not found"; // Provide feedback if image fails to load
-};
+// img.onerror = function() {
+//   console.error("Error loading image. Check the image source.");
+//   img.alt = "Image not found"; // Provide feedback if image fails to load
+// };
 
 // Adding hover effect using mouse events
 div.addEventListener('mouseenter', () => {
@@ -44,11 +54,12 @@ div.addEventListener('mouseleave', () => {
   div.style.transform = 'scale(1)';      // Reset size
 });
 
-div.appendChild(img);
-// div.appendChild(button);
+div.appendChild(button);
+button.appendChild(img);
 
 document.body.appendChild(div);
 
 button.addEventListener('click', function() {
-  alert("Button clicked!");
+  chrome.action.openPopup();
+  // alert("Button clicked!");
 });
